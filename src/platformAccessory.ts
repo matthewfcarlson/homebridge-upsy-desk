c6e344d705905a0fc3ba4c43ae445288199e134f
Matthew Carlson <matthewfcarlson+github@gmail.com>
2023-02-26 14:15:12 -0600

    0.3.4

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,7 +1,7 @@
 import EventSource from 'eventsource';
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { UpsyDeskPlatform } from './platform';
-import http from 'http';
+// import http from 'http';
 // eslint-disable-next-line max-len
 import { UpsyDeskDeviceAccessoryContext, UpsyDeskStatePacketButtonZ, UpsyDeskStatePacketNumberZ, UpsyDeskStatePacketSelectZ, UpsyDeskStatePacketSensorZ } from './upsy_types';
 
